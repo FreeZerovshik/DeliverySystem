b153c312372425011014667dd89cad82c3bbe312
Popchenko Matvey <matvei.popchenko@gmail.com>
2018-10-29 12:29:32 +0300

    add file dialog

---
+++ DeliverySystem/FMain.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace DeliverySystem
+{
+    partial class FMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FMain));
+            this.bOpenFile = new System.Windows.Forms.Button();
+            this.tFilePath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bOpenFile
+            // 
+            resources.ApplyResources(this.bOpenFile, "bOpenFile");
+            this.bOpenFile.Name = "bOpenFile";
+            this.bOpenFile.UseVisualStyleBackColor = true;
+            this.bOpenFile.Click += new System.EventHandler(this.bOpenFile_Click);
+            // 
+            // tFilePath
+            // 
+            resources.ApplyResources(this.tFilePath, "tFilePath");
+            this.tFilePath.Name = "tFilePath";
+            // 
+            // FMain
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tFilePath);
+            this.Controls.Add(this.bOpenFile);
+            this.Name = "FMain";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button bOpenFile;
+        private System.Windows.Forms.TextBox tFilePath;
+    }
+}
+
---
+++ DeliverySystem/FMain.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace DeliverySystem
+{
+    public partial class FMain : Form
+    {
+        public FMain()
+        {
+            InitializeComponent();
+        }
+
+        private void bOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileD = new OpenFileDialog();
+            openFileD.Filter = "Mdb Files|*.mdb";
+            openFileD.Title = "Select a Mdb File";
+
+            // Show the Dialog.  
+            // If the user clicked OK in the dialog and  
+            // a .CUR file was selected, open it.  
+            if (openFileD.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // Assign the cursor in the Stream to the Form's Cursor property.  
+                tFilePath.Text = openFileD.FileName;
+            }
+        }
+    }
+}
